d53c53abad7f37658a9023bb0df3cff9c3b52ad3
Nico <touboulnicolas@gmail.com>
2021-09-30 16:51:33 +0200

    tp1 et tp2 de javascript

---
+++ tp_js/script_tp_1.js
@@ -0,0 +1,10 @@
+alert("Bienvenue sur la page de formulaire");
+
+let nb1 = parseInt(prompt("Rentrez le premier nombre"));
+let nb2 = parseInt(prompt("Rentrez le deuxième nombre"));
+
+console.log(nb1, nb2);
+
+const somme = nb1 + nb2;
+
+alert(`La somme du premier nombre et du deuxieme nombre ${somme}`);
---
+++ tp_js/script_tp_2.js
@@ -0,0 +1,6 @@
+let distance = prompt("Veuillez entrer une distance en mètre");
+let temps = prompt("Veuillez entrer une durée en seconde");
+
+let vitesse = distance / temps;
+
+alert(`La vitesse est de ${vitesse} m/s `);
