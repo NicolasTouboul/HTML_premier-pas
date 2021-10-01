99180de0d62bec7241f3681fc653576f2ae17811
Nico <touboulnicolas@gmail.com>
2021-10-01 14:16:59 +0200

    boucle do while

--- tp_js/loops.js
+++ tp_js/loops.js
@@ -23,3 +23,31 @@ const noms = "Nicolas Touboul";
 for (let i = 0; i < noms.length; i++) {
   console.log(noms[i]);
 }
+
+//while{}
+
+let monmdp;
+
+while (mdp.length < 8) {
+  monmdp = prompt("Veuillez saisir un mdp: ");
+  if (monmdp.length < 8) {
+    alert(
+      "Le mot de passe est trop court, veuillez saisir un mot de passe d'au moins 8 caractères"
+    );
+  } else {
+    alert("Mot de passe enregistré");
+  }
+}
+
+//do{} while()
+
+do {
+  monmdp = prompt("Veuillez saisir un mdp: ");
+  if (monmdp.length < 8) {
+    alert(
+      "Le mot de passe est trop court, veuillez saisir un mot de passe d'au moins 8 caractères"
+    );
+  } else {
+    alert("Mot de passe enregistré");
+  }
+} while (mdp.length < 8);
