bc4f21937c7b1248572b61cb2210c850546f6d86
Nico <touboulnicolas@gmail.com>
2021-10-05 15:03:33 +0200

    correction tp 21 et 22

--- tp_js/script_tp_21.js
+++ tp_js/script_tp_21.js
@@ -29,3 +29,62 @@ for (let i = 1; i <= nbetudiant; i++) {
 for (let i = 0; i < etudiants.length; i++) {
   console.log(etudiants[i]);
 }
+
+Etudiant.prototype.changeNom = function (nouveauNom) {
+  return (this.nom = nouveauNom);
+};
+Etudiant.prototype.changePrenom = function (nouveauPrenom) {
+  return (this.prenom = nouveauPrenom);
+};
+Etudiant.prototype.changeOption = function (nouvOption) {
+  return (this.option = nouvOption);
+};
+
+const russes = [];
+const prenom_fin_A = [];
+const nom_debut_K_Femme = [];
+const nom_debut_M_Homme = [];
+let nbChinoise = 0;
+
+for (let i = 0; i < etudiants.length; i++) {
+  if (etudiants[i].pays === "Russie") {
+    russes.push(etudiants[i]);
+  }
+}
+
+for (let i = 0; i < etudiants.length; i++) {
+  if (etudiants[i].pays === "Chine" && etudiants[i].genre === "Mlle") {
+    nbChinoise++;
+  }
+}
+console.log(`le nombre d'élèves chinoise est ${nbChinoise}`);
+
+for (let i = 0; i < etudiants.length; i++) {
+  if (etudiants[i].genre === "Mr" && etudiants[i].nom[0] === "M") {
+    nom_debut_M_Homme.push(etudiants[i]);
+  }
+}
+console.log("la liste des hommes ayant un nom qui commence par M: ");
+console.log(nom_debut_M_Homme);
+console.log("________");
+
+for (let i = 0; i < etudiants.length; i++) {
+  if (etudiants[i].genre === "Mlle" && etudiants[i].nom[0] === "K") {
+    nom_debut_K_Femme.push(etudiants[i]);
+  }
+}
+console.log("la liste des femmes ayant un nom qui commence par K: ");
+console.log(nom_debut_K_Femme);
+console.log("________");
+
+for (let i = 0; i < etudiants.length; i++) {
+  if (etudiants[i].prenom[etudiants[i].prenom.length - 1] === "a") {
+    prenom_fin_A.push(etudiants[i]);
+  }
+}
+console.log("la liste des élèves ayant un prénom qui fini par a: ");
+console.log(prenom_fin_A);
+
+for (let i = 0; i < etudiants.length; i++) {
+  console.log(etudiants[i].nom);
+}
--- tp_js/script_tp_22.js
+++ tp_js/script_tp_22.js
@@ -2,31 +2,44 @@ const etudiants = [
   {
     id: 1,
     nom: "Touboul",
-    prenom: "Nicolas",
+    prenom: "Nicola",
     pays: "France",
     genre: "Mr",
   },
   {
     id: 2,
-    nom: "Bossard",
-    prenom: "Benjamin",
-    pays: "Chine",
+    nom: "Mickey",
+    prenom: "Mouse",
+    pays: "Russie",
     genre: "Mr",
   },
   {
     id: 3,
     nom: "Histane",
     prenom: "Salma",
-    pays: "Russie",
+    pays: "Chine",
+    genre: "Mlle",
+  },
+  {
+    id: 4,
+    nom: "Martin",
+    prenom: "Dona",
+    pays: "Chine",
+    genre: "Mr",
+  },
+  {
+    id: 5,
+    nom: "Kennedy",
+    prenom: "Sarah",
+    pays: "Chine",
     genre: "Mlle",
   },
 ];
-
 const russes = [];
 const prenom_fin_A = [];
-const nom_debut_K_F = [];
-const nom_debut_M_H = [];
-let nbChinois = 0;
+const nom_debut_K_Femme = [];
+const nom_debut_M_Homme = [];
+let nbChinoise = 0;
 
 for (let i = 0; i < etudiants.length; i++) {
   if (etudiants[i].pays === "Russie") {
@@ -35,29 +48,37 @@ for (let i = 0; i < etudiants.length; i++) {
 }
 
 for (let i = 0; i < etudiants.length; i++) {
-  if (etudiants[i].pays === "Chine") {
-    nbChinois++;
+  if (etudiants[i].pays === "Chine" && etudiants[i].genre === "Mlle") {
+    nbChinoise++;
   }
 }
-console.log(nbChinois);
+console.log(`le nombre d'élèves chinoise est ${nbChinoise}`);
 
 for (let i = 0; i < etudiants.length; i++) {
   if (etudiants[i].genre === "Mr" && etudiants[i].nom[0] === "M") {
-    nom_debut_M_H.push(etudiants[i]);
+    nom_debut_M_Homme.push(etudiants[i]);
   }
 }
+console.log("la liste des hommes ayant un nom qui commence par M: ");
+console.log(nom_debut_M_Homme);
+console.log("________");
 
 for (let i = 0; i < etudiants.length; i++) {
   if (etudiants[i].genre === "Mlle" && etudiants[i].nom[0] === "K") {
-    nom_debut_K_F.push(etudiants[i]);
+    nom_debut_K_Femme.push(etudiants[i]);
   }
 }
+console.log("la liste des femmes ayant un nom qui commence par K: ");
+console.log(nom_debut_K_Femme);
+console.log("________");
 
 for (let i = 0; i < etudiants.length; i++) {
   if (etudiants[i].prenom[etudiants[i].prenom.length - 1] === "a") {
     prenom_fin_A.push(etudiants[i]);
   }
 }
+console.log("la liste des élèves ayant un prénom qui fini par a: ");
+console.log(prenom_fin_A);
 
 for (let i = 0; i < etudiants.length; i++) {
   console.log(etudiants[i].nom);
